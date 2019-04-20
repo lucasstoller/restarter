8a763e2f5536a8db93b1e4f3484368a1af20bc41
Lucas Stoller <lucasstoller@hotmail.com>
2019-04-20 00:20:52 -0300

    feat: description option
    - Add description option that show the app logo
    - Also create up option

--- cli.js
+++ cli.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
 
-console.log('Here we go!');
+var program = require('commander');
+var up      = require('./app/up');
+
+program
+  .version('1.0.0', '-v, --version')
+  .option('up <setup>', 'Installs all the programs and plugins in <setup> file')
+  .parse(process.argv);
+
+if (process.argv.length == 2) {
+  console.log(`                     . .. .                      
+                 ::/oysssssyyo/::                 
+             -+/+sho:.   hy .:+ys+/+-             
+           /os/yy+s+    .h/     -syoys/           
+         +ooo+d/ -hs ...oo-..     :d+soo+         
+       /oyhy:d/  -/o/s+o//+ss//:.  :d/yyys:       
+      +os++/sy.::oh+/+o+ss+/odshs/- sy/+oys+      
+     +//s+:+hsyoydd/+++ymmy+/yossyyyod+:+s//+     
+      :shd//sy:oss+sh++ssso+sysho-  sy/+sds:      
+       -oshy/d:oh-:ysso/o++oo::.   -d/sos+-       
+         /sys+ho    +/:.../so+-.  -h+oss/         
+           /+o/y/:oy:/s.    .+o+++yoys:           
+             .:+//so. sy     ./s+/++.             
+                 --/osooo++oso/--                 
+                      .    .
+
+                   RESTARTER JS 👁
+                      `);
+} else {
+  if (program.up) up(program.up);
+}
