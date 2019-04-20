2f4a22fd64652efc63dd03dc8d4b8fec5f5cd70d
Lucas Stoller <lucasstoller@hotmail.com>
2019-04-20 16:51:30 -0300

    feat: Up option
    -  Create 'up <setup>' that install all programas inside <setup> (.json file)

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # RESTARTER JS
-## A javascript tool to fast reset your station
+## A Multiplataform javascript tool to fast reset your station
+![Dr. Strange](https://i.ytimg.com/vi/OYEfs8hUUu4/maxresdefault.jpg)
+### Requirements:
+* Windows
+  - [Chocolatey Package Manager]('https://chocolatey.org/') Installed.
+* MacOS
+  - [Homebrew Package Manager]('https://brew.sh') Installed.
+* Linux
+  - [Apt Package Manager](https://askubuntu.com/questions/860375/installing-apt-get') installed.
+
+### Usage
+* Clone this repo
+* Run `npm link` inside the repo to create a symlink
+* Run `restarter <option>` and be happy
+
+### Options
+* `up <setup>`:
+  - Install all programs represented inside `<setup>` file.
+
+#### Setup File
+* Your setup image file. It's a JSON (.json) file that constains all your programs described.
+* Example: `studio.json`
+```
+{
+  "type": "__restarter",
+  "programs":[
+    {"name":"atom"},
+    {"name":"zenkit"},
+    {"name":"audacity"}
+  ]
+}
+```
---
+++ app/up.js
@@ -0,0 +1,87 @@
+// REQUIRES
+const fs = require('fs');
+const shell = require('shelljs');
+
+// CONST
+const OS = process.platform;
+const PACKAGE_MANAGERS = {
+  windows: {
+    name: "Chocolatey", 
+    command: "choco",
+    site: "https://chocolatey.org/"
+  },
+  linux: {
+    name: "Apt", 
+    command: "apt",
+    site: "https://askubuntu.com/questions/860375/installing-apt-get"
+  },
+  macos: {
+    name: "Homebrew",
+    command: "brew",
+    site: "https://brew.sh"
+  }
+}
+
+// FUNCTIONS
+function commandExists(command){
+  return shell.which(command);
+}
+
+function noPackageManagerAlert(os){
+  console.log(`
+    Restarter does not work on ${os} systems without ${PACKAGE_MANAGERS[os].name}.\n
+    Please access ${PACKAGE_MANAGERS[os].site} to install it.
+  `);
+}
+
+function install(programs, os){
+  if (programs.length == 0) return;
+  if (programs.length >= 1){
+    program = programs.shift();
+    const manager = os.command
+    const command = shell.exec(`${manager} install ${program.name} --yes`, {silent: true})
+    
+    if (command.code !== 0) {
+      shell.echo(`✖ ️Error: ${program.name} could not be istalled.`);
+    } else {
+      shell.echo(`✔️ ️${program.name} installed.`);
+    }
+
+    install(programs, os);
+  }
+}
+
+// UP ITSELFES
+module.exports = path => {
+  if (!fs.existsSync(path)) return console.log('Error! File does not exists.');
+  if (!path.match(/(.json)$/)) return console.log('Error! Not supported format.');
+
+  const rawdata = fs.readFileSync(path);
+  const setup = JSON.parse(rawdata);
+  if (setup.type != "__restarter") return console.log('\nError! Not a restarter file.\n');
+  
+  console.log(`Setuping ${path} station...`);
+
+  switch (OS) {
+    case 'win32':
+      if (!commandExists(PACKAGE_MANAGERS.windows.command)) noPackageManagerAlert('windows');
+      else {
+        install(setup.programs, PACKAGE_MANAGERS.windows);
+      }
+      break;
+      
+    case 'linux':
+      if (!commandExists(PACKAGE_MANAGERS.linux.command)) noPackageManagerAlert('linux');
+      else {
+        install(setup.programs, PACKAGE_MANAGERS.linux);
+      }
+      break;
+
+    case 'darwin':
+      if (!commandExists(PACKAGE_MANAGERS.macos.command)) noPackageManagerAlert('macos');
+      else {
+        install(setup.programs, PACKAGE_MANAGERS.macos);
+      }
+      break;
+  }
+}
